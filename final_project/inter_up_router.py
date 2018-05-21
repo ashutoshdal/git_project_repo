9debbdc4b19a2b0827330a9f36e68f0d070b20cb
ashutoshdal <ashutosh@dal.ca>
2018-05-21 10:46:40 -0700

    cisco file directort added

---
+++ final_project/inter_up_router.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+ 
+
+
+
+#importing connecthandler to telnet or connect with router
+
+import netmiko from connecthandler
+
+
+#dictonery created and providing the info about device
+cisco_1700{ 
+'Device_type':'cisco_ios_telnet',
+'ip':'192.168.11.1',
+'port':'2001',
+'username':'root',
+'password':'cisco'
+}
+
+
+ 
+
